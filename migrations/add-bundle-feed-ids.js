e0f7fed80ac561673a11310b6a051a674efaaf37
Matthew Wigginton Conway <matt@indicatrix.org>
2016-06-23 11:40:25 -0400

    check for null feeds before migrating bundle.

--- migrations/add-bundle-feed-ids.js
+++ migrations/add-bundle-feed-ids.js
@@ -1,6 +1,8 @@
 // Add IDs to bundle.feeds
 
 db.bundles.find().forEach(function (b) {
-  b.feeds.forEach(function (f) { f.id = f.feedId + '_' + b._id })
-  db.bundles.save(b)
+  if (b.feeds != null) {
+    b.feeds.forEach(function (f) { f.id = f.feedId + '_' + b._id })
+    db.bundles.save(b)
+  }
 })
