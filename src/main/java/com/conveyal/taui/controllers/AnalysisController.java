31827df8c74f24cce3c3b4d18f271a7c717bbf02
Matthew Wigginton Conway <matt@indicatrix.org>
2016-10-11 08:53:01 -0400

    better logging.

--- src/main/java/com/conveyal/taui/controllers/AnalysisController.java
+++ src/main/java/com/conveyal/taui/controllers/AnalysisController.java
@@ -1,6 +1,8 @@
 package com.conveyal.taui.controllers;
 
 import com.conveyal.taui.AnalystConfig;
+import com.google.common.io.ByteStreams;
+import com.google.common.io.CountingInputStream;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
@@ -10,6 +12,9 @@ import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.ConnectException;
 import java.net.SocketTimeoutException;
 
@@ -21,40 +26,46 @@ import static spark.Spark.*;
 public class AnalysisController {
     public static final Logger LOG = LoggerFactory.getLogger(AnalysisController.class);
 
-    public static String analysis (Request req, Response res) throws UnirestException {
+    public static byte[] analysis (Request req, Response res) throws UnirestException {
         // we already know the user is authenticated, and we need not check if they have access to the graphs etc,
         // as they're all coded with UUIDs which contain significantly more entropy than any human's account password.
         String path = req.pathInfo().replaceAll("^/api/analysis/", "");
         String method = req.requestMethod();
 
-        res.type("application/json");
-
         String brokerUrl = AnalystConfig.offline ? "http://localhost:6001" : AnalystConfig.brokerUrl;
+
+        HttpResponse<InputStream> brokerRes;
 
         try {
             if ("GET".equals(method)) {
-                HttpResponse<String> brokerRes = Unirest.get(brokerUrl + "/" + path)
-                        .asString();
-
-                res.status(brokerRes.getStatus());
-                return brokerRes.getBody();
+                brokerRes = Unirest.get(brokerUrl + "/" + path)
+                        .asBinary();
             } else if ("POST".equals(method)) {
-                HttpResponse<String> brokerRes = Unirest.post(brokerUrl + "/" + path)
+                brokerRes = Unirest.post(brokerUrl + "/" + path)
                         .body(req.body())
-                        .asString();
-
-                res.status(brokerRes.getStatus());
-                return brokerRes.getBody();
+                        .asBinary();
             } else if ("DELETE".equals(method)) {
-                HttpResponse<String> brokerRes = Unirest.delete(brokerUrl + "/" + path)
-                        .asString();
-
-                res.status(brokerRes.getStatus());
-                return brokerRes.getBody();
+                brokerRes = Unirest.delete(brokerUrl + "/" + path)
+                        .asBinary();
             } else {
                 halt(400, "Unsupported method for broker request");
                 return null;
             }
+
+            res.status(brokerRes.getStatus());
+            res.type(brokerRes.getHeaders().getFirst("Content-Type"));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            InputStream is = new CountingInputStream(brokerRes.getBody());
+
+            try {
+                long l = ByteStreams.copy(is, baos);
+                LOG.info("Returning {} bytes to scenario editor frontend", l);
+                is.close();
+            } catch (IOException e) {
+                LOG.error("Error proxying broker", e);
+            }
+
+            return baos.toByteArray();
         } catch (UnirestException e) {
             LOG.error("analysis error", e.getCause());
             if (e.getCause() instanceof SocketTimeoutException) {
