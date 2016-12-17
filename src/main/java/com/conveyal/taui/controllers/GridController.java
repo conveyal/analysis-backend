f0a0a1702a8c06894728253d50c4fcf069d41ce8
Matthew Wigginton Conway <matt@indicatrix.org>
2016-12-17 09:44:01 -0500

    allow shapefile upload.

--- src/main/java/com/conveyal/taui/controllers/GridController.java
+++ src/main/java/com/conveyal/taui/controllers/GridController.java
@@ -11,6 +11,7 @@ import com.conveyal.taui.grids.SeamlessCensusGridFetcher;
 import com.conveyal.taui.models.Project;
 import com.conveyal.taui.persistence.Persistence;
 import com.csvreader.CsvReader;
+import com.google.common.io.Files;
 import com.vividsolutions.jts.geom.Envelope;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
@@ -26,6 +27,7 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
@@ -79,6 +81,8 @@ public class GridController {
         return res;
     }
 
+    /** Handle many types of file upload */
+    // TODO process async
     public static List<Project.Indicator> createGrid (Request req, Response res) throws Exception {
         ServletFileUpload sfu = new ServletFileUpload(fileItemFactory);
         Map<String, List<FileItem>> query = sfu.parseParameterMap(req.raw());
@@ -88,18 +92,36 @@ public class GridController {
 
         String dataSet = query.get("Name").get(0).getString("UTF-8");
 
+        Map<String, Grid> grids = null;
+
         for (FileItem fi : query.get("files")) {
-            if (fi.getName().endsWith(".csv")) {
-                return createGridFromCsv(projectId, dataSet, query);
+            String name = fi.getName();
+            if (name.endsWith(".csv")) {
+                LOG.info("Detected grid stored as CSV");
+                grids = createGridsFromCsv(query);
+                break;
+            } else if (name.endsWith(".shp")) {
+                LOG.info("Detected grid stored as shapefile");
+                grids = createGridsFromShapefile(query, fi.getName().substring(0, name.length() - 4));
+                break;
             }
         }
 
-        halt(400);
-        return null;
+        if (grids == null) {
+            halt(400);
+            return null;
+        } else {
+            List<Project.Indicator> indicators = writeGridsToS3(grids, projectId, dataSet);
+            Project project = Persistence.projects.get(projectId).clone();
+            project.indicators = new ArrayList<>(project.indicators);
+            project.indicators.addAll(indicators);
+            Persistence.projects.put(projectId, project);
+            return indicators;
+        }
     }
 
     /** Create a grid from WGS 84 points in a CSV file */
-    private static List<Project.Indicator> createGridFromCsv(String projectId, String dataSetName, Map<String, List<FileItem>> query) throws Exception {
+    private static Map<String, Grid> createGridsFromCsv(Map<String, List<FileItem>> query) throws Exception {
         String latField = query.get("latField").get(0).getString("UTF-8");
         String lonField = query.get("lonField").get(0).getString("UTF-8");
 
@@ -114,114 +136,70 @@ public class GridController {
         File tempFile = File.createTempFile("grid", ".csv");
         file.get(0).write(tempFile);
 
-        CsvReader reader = new CsvReader(new BufferedInputStream(new FileInputStream(tempFile)), Charset.forName("UTF-8"));
-        reader.readHeaders();
-
-        String[] headers = reader.getHeaders();
-        if (!Stream.of(headers).filter(h -> h.equals(latField)).findAny().isPresent()) {
-            LOG.info("Lat field not found!");
-            halt(400, "Lat field not found");
-        }
-
-        if (!Stream.of(headers).filter(h -> h.equals(lonField)).findAny().isPresent()) {
-            LOG.info("Lon field not found!");
-            halt(400, "Lon field not found");
-        }
-
-        Envelope envelope = new Envelope();
-
-        // Keep track of which fields contain numeric values
-        Set<String> numericColumns = Stream.of(headers).collect(Collectors.toCollection(HashSet::new));
-        numericColumns.remove(latField);
-        numericColumns.remove(lonField);
-
-        int i = 0;
-        while (reader.readRecord()) {
-            if (++i % 10000 == 0) LOG.info("{} records", human(i));
-
-            envelope.expandToInclude(parseDouble(reader.get(lonField)), parseDouble(reader.get(latField)));
-
-            for (String field : numericColumns) {
-                String value = reader.get(field);
-                if (value == null || "".equals(value)) continue; // allow missing data
-                try {
-                    // TODO also exclude columns containing negatives?
-                    parseDouble(value);
-                } catch (NumberFormatException e) {
-                    numericColumns.remove(field);
-                }
-            }
-        }
-
-        reader.close();
-
-        // We now have an envelope and know which columns are numeric
-        // Make a grid for each numeric column
-        Map<String, Grid> grids = numericColumns.stream()
-                .collect(
-                        Collectors.toMap(
-                            c -> c,
-                            c -> new Grid(
-                                    SeamlessCensusGridFetcher.ZOOM,
-                                    envelope.getMaxY(),
-                                    envelope.getMaxX(),
-                                    envelope.getMinY(),
-                                    envelope.getMinX()
-                )));
-
-        // read it again, Sam - reread the CSV to get the actual values and populate the grids
-        reader = new CsvReader(new BufferedInputStream(new FileInputStream(tempFile)), Charset.forName("UTF-8"));
-        reader.readHeaders();
-
-        i = 0;
-        while (reader.readRecord()) {
-            if (++i % 10000 == 0) LOG.info("{} records", human(i));
-
-            double lat = parseDouble(reader.get(latField));
-            double lon = parseDouble(reader.get(lonField));
-
-            for (String field : numericColumns) {
-                String value = reader.get(field);
-
-                double val;
-
-                if (value == null || "".equals(value)) {
-                    val = 0;
-                } else {
-                    val = parseDouble(value);
-                }
-
-                grids.get(field).incrementPoint(lat, lon, val);
-            }
-        }
-
+        Map<String, Grid> grids = Grid.fromCsv(tempFile, latField, lonField, SeamlessCensusGridFetcher.ZOOM);
         // clean up
-        reader.close();
         tempFile.delete();
 
+        return grids;
+    }
+
+    private static Map<String, Grid> createGridsFromShapefile (Map<String, List<FileItem>> query, String baseName) throws Exception {
+        // extract relevant files: .shp, .prj, .dbf, and .shx.
+        // We need the SHX even though we're looping over every feature as they might be sparse.
+        Map<String, FileItem> filesByName = query.get("files").stream()
+                .collect(Collectors.toMap(FileItem::getName, f -> f));
+
+        if (!filesByName.containsKey(baseName + ".shp") ||
+                    !filesByName.containsKey(baseName + ".prj") ||
+                    !filesByName.containsKey(baseName + ".dbf")) {
+            halt(400, "Shapefile upload must contain .shp, .prj, and .dbf");
+        }
+
+        File tempDir = Files.createTempDir();
+
+        File shpFile = new File(tempDir, "grid.shp");
+        filesByName.get(baseName + ".shp").write(shpFile);
+
+        File prjFile = new File(tempDir, "grid.prj");
+        filesByName.get(baseName + ".prj").write(prjFile);
+
+        File dbfFile = new File(tempDir, "grid.dbf");
+        filesByName.get(baseName + ".dbf").write(dbfFile);
+
+        // shx is optional, not needed for dense shapefiles
+        if (filesByName.containsKey(baseName + ".shx")) {
+            File shxFile = new File(tempDir, "grid.shx");
+            filesByName.get(baseName + ".shx").write(shxFile);
+        }
+
+        Map<String, Grid> grids = Grid.fromShapefile(shpFile, SeamlessCensusGridFetcher.ZOOM);
+        tempDir.delete();
+        return grids;
+    }
+
+    private static List<Project.Indicator> writeGridsToS3 (Map<String, Grid> grids, String projectId, String dataSourceName) {
         // write all the grids to S3
         List<Project.Indicator> ret = new ArrayList<>();
-        for (String field : numericColumns) {
-            Grid grid = grids.get(field);
+        grids.forEach((field, grid) -> {
             String fieldKey = field.replaceAll(" ", "_").replaceAll("[^a-zA-Z0-9_\\-]+", "");
-            String sourceKey = dataSetName.replaceAll(" ", "_").replaceAll("[^a-zA-Z0-9_\\-]+", "");
+            String sourceKey = dataSourceName.replaceAll(" ", "_").replaceAll("[^a-zA-Z0-9_\\-]+", "");
             String key = String.format("%s_%s", fieldKey, sourceKey);
             String gridKey = String.format("%s/%s.grid", projectId, key);
             String pngKey = String.format("%s/%s.png", projectId, key);
 
-            grid.write(GridFetcher.getOutputStream(AnalystConfig.gridBucket, gridKey));
-            //grid.writePng(GridFetcher.getOutputStream(AnalystConfig.gridBucket, pngKey));
+            try {
+                grid.write(GridFetcher.getOutputStream(AnalystConfig.gridBucket, gridKey));
+                grid.writePng(GridFetcher.getOutputStream(AnalystConfig.gridBucket, pngKey));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
 
             Project.Indicator indicator = new Project.Indicator();
             indicator.key = key;
             indicator.name = field;
-            indicator.dataSource = dataSetName;
+            indicator.dataSource = dataSourceName;
             ret.add(indicator);
-        }
-
-        Project project = Persistence.projects.get(projectId).clone();
-        project.indicators.addAll(ret);
-        Persistence.projects.put(projectId, project);
+        });
 
         return ret;
     }
