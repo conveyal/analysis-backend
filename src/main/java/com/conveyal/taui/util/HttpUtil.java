89fc7a152c232877010e6dd5f1600d98f50796cd
Matthew Wigginton Conway <matt@indicatrix.org>
2017-02-24 12:23:40 -0500

    add timeouts to HTTP client library.

--- src/main/java/com/conveyal/taui/util/HttpUtil.java
+++ src/main/java/com/conveyal/taui/util/HttpUtil.java
@@ -1,6 +1,7 @@
 package com.conveyal.taui.util;
 
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
@@ -8,5 +9,15 @@ import org.apache.http.impl.client.HttpClients;
  * Created by matthewc on 2/15/17.
  */
 public class HttpUtil {
-    public static final CloseableHttpClient httpClient = HttpClients.createDefault();
+    private static final RequestConfig requestConfig = RequestConfig.custom()
+            .setConnectTimeout(60 * 1000)
+            .setSocketTimeout(60 * 1000)
+            .build(); // time out after one minute, prevent connections from becoming blocked.
+
+    public static final CloseableHttpClient httpClient = HttpClients.custom()
+            .setMaxConnPerRoute(1024)
+            .setMaxConnTotal(2048)
+            .setDefaultRequestConfig(requestConfig)
+            .build();
+
 }
