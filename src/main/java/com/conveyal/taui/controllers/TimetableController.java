76230d4432759fb051f77afc0cc8056c74063fb9
evansiroky <evan.siroky@yahoo.com>
2018-05-04 20:41:36 -0700

    JSONify modification segments

--- src/main/java/com/conveyal/taui/controllers/TimetableController.java
+++ src/main/java/com/conveyal/taui/controllers/TimetableController.java
@@ -47,7 +47,7 @@ public class TimetableController {
                     JSONObject m = new JSONObject();
                     m.put("_id", modification._id);
                     m.put("name", modification.name);
-                    m.put("segments", tripPattern.segments);
+                    m.put("segments", JsonUtil.objectMapper.valueToTree(tripPattern.segments));
                     JSONArray modificationTimetables = new JSONArray();
                     for (AddTripPattern.Timetable timetable : tripPattern.timetables) {
                         modificationTimetables.add(JsonUtil.objectMapper.valueToTree(timetable));
