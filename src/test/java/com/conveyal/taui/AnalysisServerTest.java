da3098a4358e9dea7f124d115f2fe5dfc1f14e32
evansiroky <evan.siroky@yahoo.com>
2018-06-13 17:50:36 -0700

    refactor(test): refactor server test to startup with test config file

--- src/test/java/com/conveyal/taui/AnalysisServerTest.java
+++ src/test/java/com/conveyal/taui/AnalysisServerTest.java
@@ -1,21 +1,21 @@
 package com.conveyal.taui;
 
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.containsString;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 /**
  * A few tests of some basic routes of the server.
  * This relies on the configuration file in the project root being a local (offline=true) configuration.
  */
 public class AnalysisServerTest {
+    private static final Logger LOG = LoggerFactory.getLogger(AnalysisServerTest.class);
+    private static boolean setUpIsDone = false;
 
     /**
      * Prepare and start a testing-specific web server
@@ -23,20 +23,42 @@ public class AnalysisServerTest {
      */
     @BeforeClass
     public static void setUp() throws Exception {
+        if (setUpIsDone) {
+            return;
+        }
+
+        // see if analysis.properties exists before testing
+        File analysisPropertiesFile = new File("analysis.properties");
+        boolean analysisPropertiesFileExistedBeforeTesting = analysisPropertiesFile.exists();
+
+        // copy actual analysis.properties so we don't overwrite it
+        if (analysisPropertiesFileExistedBeforeTesting) {
+            LOG.info("moving existing analysis.properties file");
+            Files.move(Paths.get("analysis.properties"), Paths.get("analysis.properties.original"));
+        }
+
+        // copy test version of analysis.properties to use in starting up server
+        LOG.info("moving test analysis.properties file");
+        Files.copy(
+            Paths.get("analysis.properties.test"),
+            Paths.get("analysis.properties"),
+            StandardCopyOption.REPLACE_EXISTING
+        );
+
         // Start a server using the configuration in the root of the project.
         String[] args = {};
         AnalysisServer.main(args);
-    }
 
-    /**
-     * Make sure the index page can load.
-     */
-    @Test
-    public void canReturnHtml() {
-        given()
-            .port(7070)
-            .get("/")
-        .then()
-            .body(containsString("Conveyal Analysis"));
+        // replace analysis.properties with original file
+        if (analysisPropertiesFileExistedBeforeTesting) {
+            LOG.info("moving back existing analysis.properties file");
+            Files.move(
+                Paths.get("analysis.properties.original"),
+                Paths.get("analysis.properties"),
+                StandardCopyOption.REPLACE_EXISTING
+            );
+        }
+
+        setUpIsDone = true;
     }
 }
