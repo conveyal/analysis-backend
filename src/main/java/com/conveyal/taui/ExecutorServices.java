188568b435f03f5cc8bbf296750e2fb28613871f
ansons <ansonstewart@gmail.com>
2018-07-16 17:29:12 -0400

    docs(threading): add comment on ExecutorServices

--- src/main/java/com/conveyal/taui/ExecutorServices.java
+++ src/main/java/com/conveyal/taui/ExecutorServices.java
@@ -4,9 +4,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * Shared unbounded Thread Executor for the entire server to help limit heavy concurrent operations
+ * Wrapper around two ExecutorServices, making them accessible JVM-wide. The two fields here allow a "heavy" executor
+ * (for slow tasks) and a "light" executor (for fast tasks, the "passing lane"), each with a number of threads that
+ * can be limited in the main analysis.properties configuration file to help limit heavy concurrent operations.
  */
-public class ExecutorServices {
+public abstract class ExecutorServices {
     public static final ExecutorService light = Executors.newFixedThreadPool(AnalysisServerConfig.lightThreads);
     public static final ExecutorService heavy = Executors.newFixedThreadPool(AnalysisServerConfig.heavyThreads);
 }
