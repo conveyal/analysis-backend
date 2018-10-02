6547f5a1e784c7e08440d8c7fc68a4d14694113a
Andrew Byrd <andrew@fastmail.net>
2018-10-02 23:16:21 +0800

    use distinct value for auto-generated stops' IDs, #174
    also use clear boolean variable to switch off blocks when we're not auto-generating stops

--- src/main/java/com/conveyal/taui/models/ModificationStop.java
+++ src/main/java/com/conveyal/taui/models/ModificationStop.java
@@ -15,13 +15,20 @@ import java.util.Stack;
 import java.util.stream.Collectors;
 
 class ModificationStop {
+
+    // Workaround for the fact that id = null was used for both auto-generated stops and new stops not referencing
+    // GTFS stop IDs. We give the auto-generated stops this instance as their ID, and check it with identity equality.
+    // As long as we don't let these escape from this class, there's no risk of accidentally semantically equaling a
+    // real stop ID from a feed.
+    public static final String AUTO_GENERATED_STOP_ID = "AUTO_GENERATED_STOP";
+
     private static double MIN_SPACING_PERCENTAGE = 0.25;
     private static int DEFAULT_SEGMENT_SPEED = 15;
     public static int SECONDS_PER_HOUR = 60 * 60;
     public static int METERS_PER_KM = 1000;
 
     private Coordinate coordinate;
-    private String id;
+    private String id; // null means a newly created stop that does not reference a GTFS stop
     private double distanceFromStart;
 
     private ModificationStop(Coordinate c, String id, double distanceFromStart) {
@@ -31,17 +38,17 @@ class ModificationStop {
     }
 
     /**
-     * Create the StopSpec types required by r5
-     * @param stops
-     * @return
+     * Convert ModificationStops (internal to the backend conversion process) to the StopSpec type required by r5.
      */
-    static List<StopSpec> toSpec (List<ModificationStop> stops) {
+    static List<StopSpec> toStopSpecs (List<ModificationStop> stops) {
         return stops
                 .stream()
                 .map(s -> {
-                    if (s.id == null){
+                    if (s.id == null || s.id == AUTO_GENERATED_STOP_ID){
+                        // Stop is newly created or auto-generated so does not reference a GTFS stop ID.
                         return new StopSpec(s.coordinate.x, s.coordinate.y);
                     } else {
+                        // Stop references an existing GTFS stop ID.
                         return new StopSpec(s.id);
                     }
                 })
@@ -49,11 +56,12 @@ class ModificationStop {
     }
 
     /**
-     * We don't just use `StopSpec`s here because we need to keep the `distanceFromStart` for generating hop times.
-     * @param segments Modification segments
-     * @return ModificationStop[]
+     * Ideally we'd just convert directly to R5 StopSpec instances, but we need to track distanceFromStart for
+     * generating hop times from speeds. See conveyal/analysis-backend#175 for a cleaner solution.
      */
     static List<ModificationStop> getStopsFromSegments (List<Segment> segments) {
+
+        // Keep a stack of Stops because as part of auto-generating stops we sometimes need to back one out.
         Stack<ModificationStop> stops = new Stack<>();
         CoordinateReferenceSystem crs = DefaultGeographicCRS.WGS84;
 
@@ -73,17 +81,18 @@ class ModificationStop {
         for (Segment segment : segments) {
             Coordinate[] coords = segment.geometry.getCoordinates();
             int spacing = segment.spacing;
+            boolean autoCreateStops = spacing > 0;
             for (int i = 1; i < coords.length; i++) {
                 Coordinate c0 = coords[i - 1];
                 Coordinate c1 = coords[i];
                 double distanceThisLineSegment;
                 try {
+                    // JTS orthodromic distance returns meters, considering the input coordinate system.
                     distanceThisLineSegment = JTS.orthodromicDistance(c0, c1, crs);
                 } catch (TransformException e) {
                     throw AnalysisServerException.unknown(ExceptionUtils.asString(e));
                 }
-
-                if (spacing > 0) {
+                if (autoCreateStops) {
                     // Auto-create stops while this segment includes at least one point that is more than 'spacing'
                     // meters farther along the pattern than the previously created stop
                     while (distanceToLastStop + spacing < distanceToLineSegmentStart + distanceThisLineSegment) {
@@ -97,8 +106,8 @@ class ModificationStop {
                         // last line segment until we create a new stop?
                         distanceToLastStop = distanceToLineSegmentStart + frac * distanceThisLineSegment;
 
-                        // Add the auto-created stop without an id
-                        stops.add(new ModificationStop(c, null, distanceToLastStop));
+                        // Add the auto-created stop with a special ID that can be checked with identity equality
+                        stops.add(new ModificationStop(c, AUTO_GENERATED_STOP_ID, distanceToLastStop));
                     }
                 }
 
@@ -106,10 +115,12 @@ class ModificationStop {
             }
 
             if (segment.stopAtEnd) {
-                // If the last auto-generated stop was too close to a manually created stop (other than the first stop), pop it
-                if (stops.size() > 1) {
+                // If the last auto-generated stop was too close to a manually created stop (other than the first stop),
+                // remove it.
+                if (autoCreateStops && stops.size() > 1) {
                     ModificationStop lastStop = stops.peek();
-                    if (lastStop.id == null && (distanceToLineSegmentStart - distanceToLastStop) / spacing < MIN_SPACING_PERCENTAGE) {
+                    if (lastStop.id == AUTO_GENERATED_STOP_ID
+                            && (distanceToLineSegmentStart - distanceToLastStop) / spacing < MIN_SPACING_PERCENTAGE) {
                         stops.pop();
                     }
                 }
@@ -132,10 +143,11 @@ class ModificationStop {
 
         int[] stopDwellTimes = new int[stops.size()];
 
+        // This "real" stop index is the index into the originally supplied stops, ignoring the auto-generated ones.
         int realStopIndex = 0;
         for (int i = 0; i < stops.size(); i++) {
             String id = stops.get(i).id;
-            if (id == null || dwellTimes == null || dwellTimes.length <= realStopIndex) {
+            if (id == AUTO_GENERATED_STOP_ID || dwellTimes == null || dwellTimes.length <= realStopIndex) {
                 stopDwellTimes[i] = defaultDwellTime;
             } else {
                 Integer specificDwellTime = dwellTimes[realStopIndex];
@@ -153,8 +165,9 @@ class ModificationStop {
         }
 
         int[] hopTimesSeconds = new int[stops.size() - 1];
-
         ModificationStop lastStop = stops.get(0);
+
+        // This "real" stop index is the index into the originally supplied stops, ignoring the auto-generated ones.
         int realStopIndex = 0;
         for (int i = 0; i < hopTimesSeconds.length; i++) {
             ModificationStop stop = stops.get(i + 1);
@@ -164,7 +177,7 @@ class ModificationStop {
                     DEFAULT_SEGMENT_SPEED;
             hopTimesSeconds[i] = (int) (hopDistance / (segmentSpeedKph * METERS_PER_KM) * SECONDS_PER_HOUR);
 
-            if (stop.id != null) {
+            if (stop.id != AUTO_GENERATED_STOP_ID) {
                 realStopIndex++;
             }
 
